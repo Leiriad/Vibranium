b778d1df4dcc88fc6ca7f245b828dff2e6c33856
Leiriad <leiriadjenkins@gmail.com>
2026-03-31 20:45:53 +0200

    Textures - Emissives and light removal

--- src/client/java/vibranium/VibraniumClient.java
+++ src/client/java/vibranium/VibraniumClient.java
@@ -1,10 +1,16 @@
 package vibranium;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
+import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
+import vibranium.init.VibraniumBlocks;
 
 public class VibraniumClient implements ClientModInitializer {
+
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		// Dans ta méthode onInitializeClient
+		BlockRenderLayerMap.putBlock(VibraniumBlocks.VIBRANIUM_ORE, ChunkSectionLayer.SOLID);
 	}
 }
