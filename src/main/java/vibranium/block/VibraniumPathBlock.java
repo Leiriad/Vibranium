c04f743519fe06af4c94f79fa6a3b3beba9a5ae5
Leiriad <leiriadjenkins@gmail.com>
2026-04-03 19:49:03 +0200

    Path block reverts to dirt

--- src/main/java/vibranium/block/VibraniumPathBlock.java
+++ src/main/java/vibranium/block/VibraniumPathBlock.java
@@ -1,8 +1,12 @@
 package vibranium.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.ScheduledTickAccess;
 import net.minecraft.world.level.block.DirtPathBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import vibranium.init.VibraniumBlocks;
@@ -18,5 +22,15 @@ public class VibraniumPathBlock extends DirtPathBlock {
         if (!canSurvive(state, world, pos)) {
             world.setBlockAndUpdate(pos, VibraniumBlocks.VIBRANIUM_DIRT.defaultBlockState());
         }
+    }
+
+    //Turns back to vibranium_dirt if a block is on top of it
+    @Override
+    protected BlockState updateShape(BlockState state, LevelReader world, ScheduledTickAccess scheduledTickAccess, BlockPos pos, Direction direction, BlockPos blockPos2, BlockState blockState2, RandomSource randomSource) {
+
+        if (direction == Direction.UP && !state.canSurvive(world, pos)) {
+            return VibraniumBlocks.VIBRANIUM_DIRT.defaultBlockState();
+        }
+        return super.updateShape(state, world, scheduledTickAccess, pos, direction, blockPos2, blockState2, randomSource);
     }
 }
